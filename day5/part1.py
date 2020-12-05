43f2db5e0195f64a3be5a3dccfa5521a3874f5d4
dotflac <53354133+dotflac@users.noreply.github.com>
2020-12-05 17:22:38 +0000

    day1 part1

---
+++ day5/part1.py
@@ -0,0 +1,17 @@
+def decode_boarding_pass(s):
+    print(s)
+    row, col = s[:7], s[7:]
+    row = row.replace('B','1').replace('F','0')
+    col = col.replace('R','1').replace('L','0')
+    row = int(row, base=2)
+    col = int(col, base=2)
+    return row, col, (row * 8 + col)
+
+if __name__ == '__main__':
+    ids = []
+    with open('input.txt') as f:
+        for line in f:
+            line = line.rstrip()
+            *_, id_num = decode_boarding_pass(line)
+            ids.append(id_num)
+    print(max(ids))
